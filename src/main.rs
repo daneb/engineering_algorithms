db19c20566078ae99f65bf4e4ef69de6612519fe
danebalia <dane.balia@sourcing-sa.co.za>
2020-02-02 22:37:21 +0200

    Added loading of quick sort input text

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 // https://en.wikipedia.org/wiki/Quicksort#Hoare_partition_scheme
 // https://itnext.io/a-sort-of-quick-guide-to-quicksort-and-hoares-partitioning-scheme-in-javascript-7792112c6d1
 // In place = An algorithm in which the data that is being operated on does not leave its data structure.
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
 
 fn main() {
-    let mut arr : Vec<i32> = vec![1,3,5,2,7,8,4];
+    let mut arr : Vec<i32> = read_file_to_sort().unwrap().into();
     let left = 0;
     let right = arr.len() - 1;
     let result = quicksort(&mut arr, left, right as i32, &mut 0);
@@ -20,7 +23,7 @@ fn quicksort(arr : &mut Vec<i32>, left : i32, right : i32, compares : &mut i32) 
     *compares = *compares + (index - 1);
     println!("a: {}", *compares);
     quicksort(arr, left, index - 1, compares);
-    *compares = *compares + (index - 1);
+    *compares = *compares + (right - 1);
     println!("b: {}", *compares);
     quicksort(arr, index, right, compares);
 
@@ -44,4 +47,14 @@ fn partition(arr : &mut Vec<i32>, mut left : i32, mut right : i32, pivot : i32) 
         }
     }
     return left;
+}
+
+fn read_file_to_sort() -> Result<Vec<i32>, io::Error> {
+    let mut f = File::open("QuickSort.txt")?;
+    let mut buffer = Vec::new();
+
+    match f.read_to_end(&mut buffer) {
+        Ok(_) => Ok(buffer.into_iter().map(|value| value as i32).collect()),
+        Err(e) => Err(e),
+    }
 }
