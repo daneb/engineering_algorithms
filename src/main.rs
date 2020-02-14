737e0853c18c50f56dd0fcdff77e5a2e93663422
danebalia <dane.balia@sourcing-sa.co.za>
2020-02-14 23:07:12 +0200

    Old method before converting to exactly like python

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,35 @@ use std::io;
 use std::io::prelude::*;
 use std::fs::File;
 
+static mut COMPARISONS: i32 = 0;
+
+
 fn main() {
-    //let mut arr : Vec<i32> = read_file_to_sort().unwrap().into();
-    let mut arr : Vec<i32> =vec![ 3, 8, 2, 5, 1, 4, 7, 6 ];
+    let mut arr : Vec<i32> = read_file_to_sort().unwrap().into();
+    //let mut arr : Vec<i32> =vec![ 3, 8, 2, 5, 1, 4, 7, 6 ];
     let left = 0;
     let right = arr.len() - 1;
-    quicksort(&mut arr, left, right as i32);
+    quicksort(&mut arr, left as i32, right as i32);
+    unsafe { println!("{}", COMPARISONS); }
 }
 
 fn quicksort(arr : &mut Vec<i32>, left : i32, right : i32) {
 
-    if left < right {
-
-        let pi = partition(arr, left, right);
-    
-        quicksort(arr, left, pi);
-        quicksort(arr, pi + 1, right);
+    if left >= right { 
+        return;
     }
+
+    // Update comparisons
+    unsafe { COMPARISONS += (right - left); }
+
+    // Partition
+    let i = partition(arr, left, right);
+
+    // Recursive Calls
+    quicksort(arr, left, i-2);
+    quicksort(arr, i, right);
+
+    // answers tried: 176404400 | 58893 | 176463294 | 1910516808 | 162085
 
 }
 
@@ -35,16 +47,14 @@ fn partition(arr : &mut Vec<i32>, left : i32, right : i32) -> i32 {
    let pivot : i32 = arr[left as usize];
    let mut i : i32 = left + 1;
 
-   for j in (left + 1)..right {
+   for j in (left + 1)..(right + 1) {
        if arr[j as usize] < pivot {
         arr.swap(j as usize, i as usize);
-        i = i + 1;
+        i += 1;
        }
    }
 
    arr.swap(left as usize, (i - 1) as usize);
-
-   return i;
 }
 
 fn read_file_to_sort() -> Result<Vec<i32>, io::Error> {
