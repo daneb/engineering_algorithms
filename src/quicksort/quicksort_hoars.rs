8958750fc1608afc57a2c3d47494bec11bebcb95
danebalia <dane.balia@sourcing-sa.co.za>
2020-02-15 00:14:50 +0200

    Moved to directories

---
+++ src/quicksort/quicksort_hoars.rs
@@ -0,0 +1,56 @@
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn run() {
+    let mut arr : Vec<i32> = read_file_to_sort().unwrap().into();
+    let left = 0;
+    let right = arr.len() - 1;
+    let result = quicksort(&mut arr, left, right as i32, &mut 0);
+    println!("{:?}", result);
+}
+
+fn quicksort(arr : &mut Vec<i32>, left : i32, right : i32, compares : &mut i32) -> i32 {
+
+    if left >= right { return *compares };
+
+    let pivot = arr[((left + right)/2) as usize];
+    let index = partition(arr, left, right, pivot);
+    
+    *compares = *compares + (index - 1);
+    quicksort(arr, left, index - 1, compares);
+    println!("index: {} right: {} compares {}", index, right, compares);
+    *compares = *compares + (right + 1);
+    quicksort(arr, index, right, compares);
+
+    return *compares;
+
+}
+
+fn partition(arr : &mut Vec<i32>, mut left : i32, mut right : i32, pivot : i32) -> i32  {
+    while left <= right {
+        while arr[left as usize] < pivot && left <= right {
+            left = left + 1;
+        }
+        while arr[right as usize] > pivot {
+            right = right - 1;
+        }
+
+        if left <= right {
+            arr.swap(left as usize, right as usize);
+            left = left + 1;
+            right = right - 1;
+        }
+    }
+    return left;
+}
+
+fn read_file_to_sort() -> Result<Vec<i32>, io::Error> {
+    let mut f = File::open("QuickSort.txt")?;
+    let mut buffer = Vec::new();
+
+    match f.read_to_end(&mut buffer) {
+        Ok(_) => Ok(buffer.into_iter().map(|value| value as i32).collect()),
+        Err(e) => Err(e),
+    }
+}
